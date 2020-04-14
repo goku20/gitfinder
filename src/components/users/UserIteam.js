d7141a66d0cec40c304577c24dc87747c7527ea3
unknown <guru.angel@gmail.com>
2020-04-14 11:15:13 +0200

    test

--- src/components/users/UserIteam.js
+++ src/components/users/UserIteam.js
@@ -2,7 +2,22 @@ import React, { Component } from "./node_modules/react";
 
 class UserItem extends Component {
   render() {
-    return <div>UserItem</div>;
+    return;
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1s'>
+          more
+        </a>
+      </div>
+    </div>;
   }
 }
 
