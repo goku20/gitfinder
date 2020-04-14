f79e02ca308e8d1a2e7a31ee9a4f47c9c966b11b
unknown <guru.angel@gmail.com>
2020-04-14 11:37:05 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import UserItem from "./components/users/UserItem ";
+import "./axios.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
