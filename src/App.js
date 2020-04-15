34dc16575e48a25299cabb1ff72d69f8d97ed935
unknown <guru.angel@gmail.com>
2020-04-15 14:38:57 +0200

    env2

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get(´https://api.github.com/users?client_id=$
+    const res = await axios.get(`https://api.github.com/users?client_id=$
       {process.env.REACT_APP_FINDER_CLIENT_ID}&client_secret=$
-       {process.env.REACT_APP_FINDER_CLIENT_SECRET});
+      {process.env.REACT_APP_FINDER_CLIENT_SECRET}`);
 
     this.setState({ users: res.data, loading: false });
   }
