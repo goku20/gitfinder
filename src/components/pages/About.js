187b70557a740ccd84df1e60a47b024b4303979c
unknown <guru.angel@gmail.com>
2020-05-04 23:48:51 +0200

    page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search gitfinder users</p>
+      <P>Verson: 1.0.0</P>
+    </Fragment>
+  );
+};
+
+export default About;
